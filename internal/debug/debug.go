d0da4d6c697c52dd9311f7046a78dd880c81dd16
ciiim <1225249223@qq.com>
2023-09-21 23:25:22 +0800

    重构debuglogger包

--- internal/debug/debug.go
+++ internal/debug/debug.go
@@ -6,17 +6,37 @@ import (
 
 type debug struct {
 	debugLogger *log.Logger
+	printfSlot  func(actionName string, format string, a ...any)
+	printlnSlot func(actionName string, a ...any)
 	activeDebug bool
 }
 
 var Dlog debug = debug{
+	printfSlot:  emptyFormatFn,
+	printlnSlot: emptyLineFn,
 	activeDebug: false,
 }
 
+var (
+	debugFormatFn = func(actionName string, format string, a ...any) {
+		Dlog.debugLogger.Printf(actionName+": "+format, a...)
+	}
+	emptyFormatFn = func(actionName string, format string, a ...any) {}
+
+	debugLineFn = func(actionName string, a ...any) {
+		Dlog.debugLogger.Println(actionName+": ", a)
+	}
+	emptyLineFn = func(actionName string, a ...any) {}
+)
+
 func DebugOn() {
+	if Dlog.activeDebug {
+		return
+	}
 	Dlog.activeDebug = true
+	Dlog.printfSlot = debugFormatFn
+	Dlog.printlnSlot = debugLineFn
 	Dlog.debugLogger = log.New(log.Writer(), "[DEBUG] ", log.Ltime|log.Lshortfile)
-	Dlog.debugLogger.SetFlags(log.Ltime | log.Lshortfile)
 }
 
 func IsDebug() bool {
@@ -24,7 +44,9 @@ func IsDebug() bool {
 }
 
 func (dl *debug) LogDebugf(actionName string, format string, a ...any) {
-	if dl.activeDebug {
-		dl.debugLogger.Printf(actionName+": "+format, a...)
-	}
+	dl.printfSlot(actionName, format, a...)
+}
+
+func (dl *debug) LogDebug(actionName string, a ...any) {
+	dl.printlnSlot(actionName, a...)
 }
