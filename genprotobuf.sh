458db07413e7bfcdcef7ef2b3caeabbe3716294c
ciiim <1225249223@qq.com>
2023-09-21 23:24:43 +0800

    更新protobuf生成脚本

--- genprotobuf.sh
+++ genprotobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-protoc --go_out=. --go_opt=paths=source_relative ./internal/fs/fspb/fileinfo.proto
-protoc --go_out=. --go_opt=paths=source_relative ./internal/fs/fspb/error.proto
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./internal/fs/fspb/peer_grpc.proto
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./internal/fs/fspb/file_grpc.proto
+protoc --go_out=. --go_opt=paths=source_relative ./internal/dfs/fspb/fileinfo.proto
+protoc --go_out=. --go_opt=paths=source_relative ./internal/dfs/fspb/error.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./internal/dfs/fspb/peer_grpc.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./internal/dfs/fspb/file_grpc.proto
